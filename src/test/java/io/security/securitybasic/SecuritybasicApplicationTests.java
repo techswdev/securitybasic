075e469ef03e554b16375cdd0eeeb0d6ffc01f48
=aaa <deungchon1210@gmail.com>
2021-01-27 23:04:19 +0900

    프로젝트 구성 및 의존성 추가

---
+++ src/test/java/io/security/securitybasic/SecuritybasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.securitybasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritybasicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
